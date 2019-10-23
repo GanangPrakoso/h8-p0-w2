243a00c9adcf00e10f2cc5cf3eeb0bf6fc3252ef
GanangPrakoso <ganangmanggolo@gmail.com>
2019-10-23 14:21:31 +0700

    +exercise_3

---
+++ exercise3_switchcase.js
@@ -0,0 +1,32 @@
+var date='12';
+var month='10';
+var year='432 Sebelum Hijriah';
+
+switch (month){
+    case '1': console.log(+date+', Januari '+year);
+    break;
+    case '2': console.log(+date+', Februari '+year);
+    break;
+    case '3': console.log(+date+', Maret '+year);
+    break;
+    case '4': console.log(+date+', April '+year);
+    break;
+    case '5': console.log(+date+', Mei '+year);
+    break;
+    case '6': console.log(+date+', Juni '+year);
+    break;
+    case '7': console.log(+date+', Juli '+year);
+    break;
+    case '8': console.log(+date+', Agustus '+year);
+    break;
+    case '9': console.log(+date+', September '+year);
+    break;
+    case '10': console.log(+date+', Oktober '+year);
+    break;
+    case '11': console.log(+date+', November '+year);
+    break;
+    case '12': console.log(+date+', Desember '+year);
+    break;
+    default : console.log('Tau cara nulis tanggak gak sih?');
+       
+}
