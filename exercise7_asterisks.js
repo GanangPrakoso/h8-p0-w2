8c2e425eb6a0e550292b3ee710cac4bdab5ee7b8
GanangPrakoso <ganangmanggolo@gmail.com>
2019-10-24 14:25:28 +0700

    exercise7 dan revisi nama

---
+++ exercise7_asterisks.js
@@ -0,0 +1,27 @@
+//SOAL NOMOR 1
+for(var rows1=1; rows1<=5; rows1++){
+    console.log('*');    
+}
+console.log('');
+
+//SOAL NOMOR 2
+var rows2 = 5;
+
+for (var i=1; i<= rows2; i++){ 
+    var asterisks="";
+    for (var j=1; j<=rows2; j++){
+    asterisks+='*';     
+    }
+    console.log(asterisks);    
+}
+console.log('');
+
+//SOAL NOMOR 3
+var rows3 = 5;
+var asterisks3='';
+for (var i = 1; i <= rows3; i++) {
+    asterisks3 += '*';
+    console.log(asterisks3);
+    for (var j = 1; j <= rows3; j++) {
+    }
+}
